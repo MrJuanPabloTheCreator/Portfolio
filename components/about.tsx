af1e88fa0dcf42641995ec1f187f37eb5b5a3540
MrJuanPabloTheCreator <juanpablocerdasilva@gmail.com>
2024-03-20 22:03:01 -0500

    Skills improvement

--- components/about.tsx
+++ components/about.tsx
@@ -29,12 +29,12 @@ export const About = () => {
     <section 
       id="about"
       // ref={ref}
-      className="flex flex-col items-center w-full py-24 bg-white/10 backdrop-blur-[24] scroll-mt-28 z-10 md:about-section" 
+      className="flex flex-col items-center w-full py-24 bg-white/10 backdrop-blur-[24] scroll-mt-28 z-10 about-section" 
     >
-      <h2 className="font-extrabold text-white text-center mb-5 text-5xl md:about-t">
+      <h2 className="font-extrabold text-white text-center mb-5 text-5xl about-t">
         About me
       </h2>
-      <p className="font-medium text-md md:text-2xl text-center mx-5 md:w-[1100px] text-slate-300 md:about-t">
+      <p className="font-medium text-md md:text-2xl text-center mx-5 md:w-[1100px] text-slate-300 about-t">
         After being born and raised in Santiago, Chile, I came to the United States pursuing a <span className="font-extrabold text-white">Bachelor&#39;s degree in Computer Information Systems </span> at <span className="font-extrabold text-white">Missouri Valley College</span>. 
         After four years, I am set to graduate in <span className="font-extrabold text-white">May 2024</span> and am looking forward to continuing my professional career as a creative tech enthusiast, leveraging my passion for innovation and technology to drive impactful projects and solutions.
         While I have a strong desire to achieve great things, I am also committed to turning those dreams into reality through strategic planning and goal-setting. Challenges bring out the best in me, and creativity and determination are the two words with which I identify myself the most. 
--- components/skills.tsx
+++ components/skills.tsx
@@ -1,34 +1,30 @@
-
-import { skills } from '../lib/data';
-import { Card } from './card';
+import { skills } from "@/lib/data"
 // import { useSectionInView } from "@/lib/hooks";
 
 export const Skills = () => {
   // const { ref } = useSectionInView("Skills");
-
+  
   return (
     <section 
       id="skills"
       // ref={ref}
       className="flex justify-center items-center scroll-mt-28 text-center z-10"
     >
-      <div className="flex flex-col items-center z-10 w-full">
-          <div className='flex w-full items-center justify-center'>
-            <h2 className="font-extrabold text-white/60 text-center mb-5 text-5xl">
-              Skills & Technologies
-            </h2>
-          </div> 
-          <div className='lg:w-[1190px] grid grid-cols-2 lg:grid-cols-4 gap-2 p-2 h-full rounded-2xl'>    
-          {skills.map((item) => (
-            <Card
-              key={item.skill}
-              img={item.img}
-              text={item.skill}
-              expertise={item.expertiselevel}
-              time={item.time}
-            />
-          ))}
+      <div className="flex flex-col justify-center items-center z-10 w-full">
+        <div className='flex w-full items-center justify-center'>
+          <h2 className="font-extrabold text-white/60 text-center mb-5 text-5xl">
+            Skills & Technologies
+          </h2>
+        </div> 
+        <div className='grid grid-cols-2 md:grid-cols-4 gap-5 w-full md:w-[1000px]'>
+        {skills.map((skill, index) => (
+          <div key={index} className="flex flex-col justify-center items-center p-5 bg-white/10 rounded-lg card">
+            <a className="text-white/80 text-5xl mb-2"><skill.icon /></a>
+            <p className='text-white/60 font-semibold text-md mb-[-5px]'>{skill.skill}</p>
+            <p className='text-white/60 text-sm'>{skill.time}</p>
           </div>
+        ))}
+        </div>
       </div>
     </section>
   )
